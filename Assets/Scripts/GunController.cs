7f0c660bd3ed72ba46ecc15a37b79214a5628412
Glutch <Glutch>
2017-02-22 18:10:21 +0100

    okey!

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public Transform weaponHold;
+    public Gun startingGun;
+    Gun equippedGun;
+
+    void Start() {
+        if (startingGun != null) {
+            EquipGun(startingGun);
+        }
+    }
+
+	// Use this for initialization
+	void EquipGun (Gun gunToEquip) {
+        if (equippedGun != null) {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = (Gun)Instantiate(gunToEquip, weaponHold.position, weaponHold.rotation);
+        equippedGun.transform.parent = weaponHold;
+
+    }
+
+    public void Shoot() {
+        if (equippedGun != null) {
+            equippedGun.Shoot();
+        }
+    }
+}
